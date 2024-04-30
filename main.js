1b04928e4be92dd134c88193eb18a708779cc905
Steven Muñoz <steven.munoz@ucuenca.edu.ec>
2024-04-30 04:27:50 -0500

    PrimeraVersion

---
+++ main.js
@@ -0,0 +1,68 @@
+function obtenerParametros() {
+    var parametros = {};
+    var query = window.location.search.substring(1);
+    var pares = query.split("&");
+    for (var i = 0; i < pares.length; i++) {
+      var par = pares[i].split("=");
+      parametros[par[0]] = decodeURIComponent(par[1]); // Decodificar el valor del parámetro
+    }
+    return parametros;
+  }
+
+
+   // Función para generar la receta médica
+   function generarReceta() {
+    var parametros = obtenerParametros();
+    var cuerpoPagina = document.getElementsByTagName("body")[0];
+    var receta = document.createElement("div");
+    receta.className = "receta";
+    
+    var logo = document.createElement("div");
+    logo.className = "logo";
+    logo.innerHTML = "<img src='logoHospital.png' alt='Logo'>";
+    receta.appendChild(logo);
+
+    var titulo = document.createElement("h2");
+    titulo.className = "titulo";
+    titulo.textContent = "Receta Médica";
+    receta.appendChild(titulo);
+
+    // Verificar que se hayan recibido todos los parámetros necesarios
+    if ('medicamentos' in parametros && 'dosis' in parametros && 'horario' in parametros) {
+      var medicamentos = parametros['medicamentos'].split(",");
+      var dosis = parametros['dosis'].split(",");
+      var horario = parametros['horario'].split(",");
+      
+      for (var i = 0; i < medicamentos.length; i++) {
+        var item = document.createElement("div");
+        item.className = "item";
+        var medicamentoElement = document.createElement("div");
+        medicamentoElement.className = "medicamento";
+        medicamentoElement.textContent = medicamentos[i];
+        var dosisElement = document.createElement("div");
+        dosisElement.className = "dosis";
+        dosisElement.textContent = dosis[i];
+        var horarioElement = document.createElement("div");
+        horarioElement.className = "horario";
+        horarioElement.textContent = horario[i];
+        
+        item.appendChild(medicamentoElement);
+        item.appendChild(dosisElement);
+        item.appendChild(horarioElement);
+        
+        receta.appendChild(item);
+      }
+      
+    } else {
+      var mensajeError = document.createElement("p");
+      mensajeError.textContent = "Error: No se han proporcionado todos los parámetros necesarios.";
+      receta.appendChild(mensajeError);
+    }
+    
+    cuerpoPagina.appendChild(receta);
+    // Imprimir la página automáticamente
+    window.print();
+  }
+
+  // Llamar a la función cuando la página haya cargado
+  window.onload = generarReceta;
